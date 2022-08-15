42e1ecead30b50f7d1b47f512a92b2e41e9868e2
admodev <admodevcodes@outlook.com>
2022-08-15 01:55:38 -0300

    add functions and typedefs

--- src/client.nim
+++ src/client.nim
@@ -1,4 +1,21 @@
-import os
+import os, threadpool
+import json
+
+type
+  Message* = object
+    username*: string
+    message*: string
+
+proc parseMessage* (data: string): Message =
+  let dataJson = parseJson(data)
+  result.username = dataJson["username"].getStr()
+  result.message = dataJson["message"].getStr()
+
+proc createMessage*(username, message: string): string =
+  result = $(%{
+    "username": %username,
+    "message": %message
+  }) & "\c\l"
 
 echo("Chat application started.")
 
@@ -8,3 +25,7 @@ if paramCount() == 0:
 let serverAddr = paramStr(1)
 
 echo("Connecting to ", serverAddr)
+
+while true:
+  let message = spawn stdin.readLine()
+  echo("Sending \"", ^message, "\"")
