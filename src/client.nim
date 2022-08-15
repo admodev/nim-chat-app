a40b67a3bfae7130d3ccdb42480c4c1a17a9b50f
admodev <admodevcodes@outlook.com>
2022-08-15 01:33:17 -0300

    init project

---
+++ src/client.nim
@@ -0,0 +1,10 @@
+import os
+
+echo("Chat application started.")
+
+if paramCount() == 0:
+  quit("Please specify the server address, e.g... ./client localhost")
+
+let serverAddr = paramStr(1)
+
+echo("Connecting to ", serverAddr)
